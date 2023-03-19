5efbdb336292397898c7012c561bbcb1e56cc5c7
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-19 21:04:10 +0530

    [Ayushi] Welcoming you

---
+++ ADO.Net_Employee_Payroll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Net_Employee_Payroll
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll program");
+            Console.ReadLine();
+        }
+    }
+}
